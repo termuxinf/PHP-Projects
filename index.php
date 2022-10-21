f2efc6a26bf0231d826f5996f600ddfb2414abe8
termuxinf <80803750+termuxinf@users.noreply.github.com>
2022-10-21 09:36:26 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat perhitungan laba dann rugi dengan php | www.malasngoding.com</title>
+</head>
+<body>
+	<h1>www.malasngoding.com</h1>
+	<h3>Membuat perhitungan laba rugi dengan PHP</h3>
+	<br/>
+	<form method="post" action="hitung.php">
+		<table>
+			<tr>
+				<td>Harga Jual</td>
+				<td><input type="text" name="harga"></td>
+			</tr>
+			<tr>
+				<td>Modal</td>
+				<td><input type="text" name="modal"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Hitung!"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
